7207d6601ca2fa8e44800a7970cf2feb707b6491
Dan Mills <dmills@qumulo.com>
2019-03-29 13:35:54 -0700

    Fix keybinds for osx and linux

--- common.d/source.sh
+++ common.d/source.sh
@@ -29,8 +29,8 @@ autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
-bindkey "^[[A" up-line-or-beginning-search # Up
-bindkey "^[[B" down-line-or-beginning-search # Down
+bindkey "^[OA" up-line-or-beginning-search # Up
+bindkey "^[OB" down-line-or-beginning-search # Down
 
 # Add $HOME/bin to $PATH
 export PATH=$PATH:$HOME/bin
--- osx.d/source.sh
+++ osx.d/source.sh
@@ -1,1 +1,5 @@
 export EDITOR='code --wait'
+
+# OSX has different keycodes for up and down arrows
+bindkey "^[OA" up-line-or-beginning-search # Up
+bindkey "^[OB" down-line-or-beginning-search # Down
