db63a322a9c0deb77f8913dca334048078614aee
Dan Mills <evilhamsterman@gmail.com>
2020-05-11 15:19:14 -0700

    remove legacy bits

--- osx.d/source.sh
+++
@@ -1,5 +0,0 @@
-export EDITOR='code --wait'
-
-# OSX has different keycodes for up and down arrows
-bindkey "^[OA" up-line-or-beginning-search # Up
-bindkey "^[OB" down-line-or-beginning-search # Down
