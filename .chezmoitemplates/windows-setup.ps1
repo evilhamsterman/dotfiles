e700dd6e576c887b8e05a37d17b0b774b2bbb1fe
Dan Mills <dmills@qumulo.com>
2023-09-01 11:33:54 -0700

    Add winget export

---
+++ .chezmoitemplates/windows-setup.ps1
@@ -0,0 +1,29 @@
+jeetdsouza.zoxide"
+				},
+				{
+					"PackageIdentifier": "junegunn.fzf"
+				}
+			],
+			"SourceDetails": {
+				"Argument": "https://cdn.winget.microsoft.com/cache",
+				"Identifier": "Microsoft.Winget.Source_8wekyb3d8bbwe",
+				"Name": "winget",
+				"Type": "Microsoft.PreIndexed.Package"
+			}
+		},
+		{
+			"Packages": [
+				{
+					"PackageIdentifier": "9WZDNCRFJ3PS"
+				}
+			],
+			"SourceDetails": {
+				"Argument": "https://storeedgefd.dsx.mp.microsoft.com/v9.0",
+				"Identifier": "StoreEdgeFD",
+				"Name": "msstore",
+				"Type": "Microsoft.Rest"
+			}
+		}
+	],
+	"WinGetVersion": "1.5.2201"
+}
