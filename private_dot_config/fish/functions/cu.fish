728d77d495c7f4a03f7695ea9da79918fec0bef1
Dan Mills <dmills@qumulo.com>
2023-09-01 14:43:32 -0700

    fix the date command

--- private_dot_config/fish/functions/cu.fish
+++ private_dot_config/fish/functions/cu.fish
@@ -1,4 +1,4 @@
 function cu --description "Update dotfiles and set the day updated"
     chezmoi update --apply
-    set --universal __last_chezmoi_update (date %Y%j)
+    set --universal __last_chezmoi_update (date +%Y%j)
 end
