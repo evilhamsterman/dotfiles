56a766ef55c5846582c861ae2c24cf1d2799376d
Dan Mills <dmills@qumulo.com>
2023-11-21 11:38:05 -0800

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,9 @@
 Invoke-Expression (&starship init powershell)
 $Env:SHELL = "pwsh.exe"
 $Env:EDITOR = "code --wait"
+
+function Connect-itvc {
+    Connect-Keeper
+    $l = Get-KeeperRecord  -Uid zHgtYS9mnRlCOFVj10fvuA
+    Connect-VIServer it-vc.corp.qumulo.com -User dmills -Password $(($l.fields | Where-Object { $_.fieldName -eq "password" }).values)
+}
