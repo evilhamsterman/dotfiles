138bf0d55ae39739f13c55efb24b9af1f67d81e9
Dan Mills <evilhamsterman@gmail.com>
2025-12-31 13:24:25 -0800

    Update .config/fish/functions/cu.fish

--- private_dot_config/fish/functions/cu.fish
+++ private_dot_config/fish/functions/cu.fish
@@ -1,4 +1,7 @@
 function cu --description "Update dotfiles and set the day updated"
-    chezmoi update --apply
-    set --universal __last_chezmoi_update (date +%Y%j)
+    # Only run if the shell is interactive to avoid automation from stalling
+    if status --is-interactive
+        chezmoi update --apply
+        set --universal __last_chezmoi_update (date +%Y%j)
+    end
 end
